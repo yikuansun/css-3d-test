adf26e1c2498a2c387b320f7ca5e1d95d138bf63
yikuansun <yikuansun2015@gmail.com>
2026-01-22 11:06:53 -0500

    Physics
    I should probably check corner & edge collision at some point but I'm lazy lmao

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
     let camera = $state({
         x: 0,
-        y: 0,
+        y: -50,
         z: 0,
         angleX: 0,
         angleY: 0,
@@ -13,6 +13,8 @@
         xOffset: 480,
         yOffset: 270,
     });
+
+    let playerVelocity = $state([0, 0, 0]); // x, y, z
 
     const TEXTURE_NAMES: { [key: number]: string } = {
         1: "grass",
@@ -28,6 +30,9 @@
     const TRANSLATION_SPEED = 200; // px / sec
     const ROTATION_SPEED = 150; // deg / sec
 
+    const GRAVITY = 800;
+    const JUMP_SPEED = 500;
+
     let lastTime: number;
     let deltaTime = $state(0);
     let displayedFPS: number = $state(0);
@@ -39,21 +44,63 @@
         lastTime = startTime;
         // REMEMBER: multiply all speeds by deltaTime
 
+        playerVelocity[0] = 0;
+        playerVelocity[2] = 0;
         if (keysPressed["w"]) {
-            camera.z -= TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180) * deltaTime;
-            camera.x += TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180) * deltaTime;
+            playerVelocity[2] -= TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180);
+            playerVelocity[0] += TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180);
         }
         if (keysPressed["s"]) {
-            camera.z += TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180) * deltaTime;
-            camera.x -= TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180) * deltaTime;
+            playerVelocity[2] += TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180);
+            playerVelocity[0] -= TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180);
         }
         if (keysPressed["a"]) {
-            camera.x -= TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180) * deltaTime;
-            camera.z -= TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180) * deltaTime;
+            playerVelocity[0] -= TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180);
+            playerVelocity[2] -= TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180);
         }
         if (keysPressed["d"]) {
-            camera.x += TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180) * deltaTime;
-            camera.z += TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180) * deltaTime;
+            playerVelocity[0] += TRANSLATION_SPEED * Math.cos(camera.angleY * Math.PI / 180);
+            playerVelocity[2] += TRANSLATION_SPEED * Math.sin(camera.angleY * Math.PI / 180);
+        }
+
+        let blockBeneathPlayer = worldPositionToGridCoord(camera.x, camera.y + 3 * CUBE_SIZE / 2 + 5, camera.z);
+        if (worldMatrix?.[blockBeneathPlayer[0]]?.[blockBeneathPlayer[1]]?.[blockBeneathPlayer[2]] !== 0) {
+            //playerVelocity[1] = 0;
+            if (keysPressed[" "]) {
+                playerVelocity[1] = -JUMP_SPEED;
+            }
+        }
+
+        playerVelocity[1] += GRAVITY * deltaTime;
+
+        camera.x += playerVelocity[0] * deltaTime;
+        camera.y += playerVelocity[1] * deltaTime;
+        camera.z += playerVelocity[2] * deltaTime;
+        let playerXEdges = [
+            worldPositionToGridCoord(camera.x - CUBE_SIZE / 2, camera.y, camera.z),
+            worldPositionToGridCoord(camera.x + CUBE_SIZE / 2, camera.y, camera.z),
+            worldPositionToGridCoord(camera.x - CUBE_SIZE / 2, camera.y + CUBE_SIZE, camera.z),
+            worldPositionToGridCoord(camera.x + CUBE_SIZE / 2, camera.y + CUBE_SIZE, camera.z),
+        ];
+        let playerYEdges = [
+            worldPositionToGridCoord(camera.x, camera.y - CUBE_SIZE / 2, camera.z),
+            worldPositionToGridCoord(camera.x, camera.y + 3 * CUBE_SIZE / 2, camera.z),
+        ];
+        let playerZEdges = [
+            worldPositionToGridCoord(camera.x, camera.y, camera.z - CUBE_SIZE / 2),
+            worldPositionToGridCoord(camera.x, camera.y, camera.z + CUBE_SIZE / 2),
+            worldPositionToGridCoord(camera.x, camera.y + CUBE_SIZE, camera.z - CUBE_SIZE / 2),
+            worldPositionToGridCoord(camera.x, camera.y + CUBE_SIZE, camera.z + CUBE_SIZE / 2),
+        ];
+        if (playerXEdges[0][0] < 0 || playerXEdges[1][0] >= worldMatrix.length || worldMatrix?.[playerXEdges[0][0]]?.[playerXEdges[0][1]]?.[playerXEdges[0][2]] !== 0 || worldMatrix?.[playerXEdges[1][0]]?.[playerXEdges[1][1]]?.[playerXEdges[1][2]] !== 0 || worldMatrix?.[playerXEdges[2][0]]?.[playerXEdges[2][1]]?.[playerXEdges[2][2]] !== 0 || worldMatrix?.[playerXEdges[3][0]]?.[playerXEdges[3][1]]?.[playerXEdges[3][2]] !== 0) {
+            camera.x -= playerVelocity[0] * deltaTime;
+        }
+        if (playerYEdges[0][1] < 0 || playerYEdges[1][1] >= worldMatrix[0].length || worldMatrix?.[playerYEdges[0][0]]?.[playerYEdges[0][1]]?.[playerYEdges[0][2]] !== 0 || worldMatrix?.[playerYEdges[1][0]]?.[playerYEdges[1][1]]?.[playerYEdges[1][2]] !== 0) {
+            camera.y -= playerVelocity[1] * deltaTime;
+            playerVelocity[1] = 0;
+        }
+        if (playerZEdges[0][2] < 0 || playerZEdges[1][2] >= worldMatrix[0][0].length || worldMatrix?.[playerZEdges[0][0]]?.[playerZEdges[0][1]]?.[playerZEdges[0][2]] !== 0 || worldMatrix?.[playerZEdges[1][0]]?.[playerZEdges[1][1]]?.[playerZEdges[1][2]] !== 0 || worldMatrix?.[playerZEdges[2][0]]?.[playerZEdges[2][1]]?.[playerZEdges[2][2]] !== 0 || worldMatrix?.[playerZEdges[3][0]]?.[playerZEdges[3][1]]?.[playerZEdges[3][2]] !== 0) {
+            camera.z -= playerVelocity[2] * deltaTime;
         }
 
         /*// camera rotation
